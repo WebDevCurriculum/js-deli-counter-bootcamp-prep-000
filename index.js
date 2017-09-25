b99317ac5f1b7f01ce984396623ffbc26e7cedc1
Charity Youngblood <youngblood.charity@gmail.com>
2017-09-25 17:29:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,9 +31,7 @@ function currentLine(line) {
     var customer = "";
     for(var i = 0; i < line.length; i++){
         customer +=  `${i + 1}. ${line[i]}, `;
-        if(i == line.length) {
-        customer += `${i + 1}. ${line[i]}`;
-    }
+        
   }
   if(i == line.length) {
   customer += `${i + 1}. ${line[i]}`;
